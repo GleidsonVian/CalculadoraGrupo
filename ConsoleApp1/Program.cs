c0401373fd0616f1c142ee67a01844af46a93174
HugoMDKK <victorhugo0856@gmail.com>
2025-03-25 23:05:03 -0300

    Primeira atualização

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,15 @@
 {
     public class Program
     {
-        public static void Main(string[] args) { }
+        public static void Main(string[] args) 
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            public int Soma(int n, int m) 
+        {
+            return x = n + m;
+        }
+    
+        }
     }
 }
