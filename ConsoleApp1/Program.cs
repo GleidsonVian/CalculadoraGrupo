c770491d3ed08e03cf4ac7e59898def7df35e4d4
HugoMDKK <victorhugo0856@gmail.com>
2025-03-25 23:05:51 -0300

    Primeiro commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,7 @@
     public class Program
     {
         public static void Main(string[] args) 
-        {
+        {/
             int n = int.Parse(Console.ReadLine());
             int m = int.Parse(Console.ReadLine());
             public int Soma(int n, int m) 
